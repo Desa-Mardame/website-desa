3229c6c6359cf7add7ff3a8154796e54d330bf9e
Hafsa-tech42 <hafsanurhalima5@gmail.com>
2024-11-04 22:20:42 -0800

    Menambahkan folder assets dan file script.js

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+ document.addEventListener("DOMContentLoaded", function() {
+       document.querySelector("main").style.opacity = 0;
+       setTimeout(() => {
+           document.querySelector("main").style.transition = "opacity 1s";
+           document.querySelector("main").style.opacity = 1;
+       }, 100);
+   });
+   
